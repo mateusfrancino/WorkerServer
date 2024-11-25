424354a52d776ba30f92309ad2372d8d78984f4c
Mateus Félix Meta <160603410+mateusfelixmeta@users.noreply.github.com>
2024-11-25 15:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ Core/Entities.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkerServer.Core.Entities;
+
+public class AtualizacaoLog
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string LojaId { get; set; }
+    public string PDVId { get; set; }
+    public string Versao { get; set; }
+    public string Status { get; set; }
+    public DateTime DataHora { get; set; }
+}
